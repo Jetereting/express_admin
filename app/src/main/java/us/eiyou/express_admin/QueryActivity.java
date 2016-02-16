d9fa9107723a860043f3f3df7c3e52218b1d4526
Au <1570967070@qq.com>
2016-02-16 16:02:20 +0800

    TableLayout更加漂亮，可左右滑动

--- app/src/main/java/us/eiyou/express_admin/QueryActivity.java
+++ app/src/main/java/us/eiyou/express_admin/QueryActivity.java
@@ -26,31 +26,37 @@ public class QueryActivity extends AppCompatActivity {
         final TableLayout tableLayout = (TableLayout) findViewById(R.id.tl);
         tableLayout.setStretchAllColumns(true);
         BmobQuery<Express_info> bmobQuery = new BmobQuery<>();
+        bmobQuery.order("-createdAt");
         bmobQuery.findObjects(getApplicationContext(), new FindListener<Express_info>() {
             @Override
             public void onSuccess(List<Express_info> list) {
                 Toast.makeText(getApplicationContext(), list.size() + "", Toast.LENGTH_LONG).show();
                 for (int row = 0; row < list.size(); row++) {
                     TableRow tableRow = new TableRow(getApplicationContext());
-
                     Express_info express_info = list.get(row);
                     TextView textView = new TextView(getApplicationContext());
+                    textView.setPadding(0,0,10,0);
                     textView.setText(express_info.getCreatedAt().substring(2,10));
                     tableRow.addView(textView);
                     TextView textView0 = new TextView(getApplicationContext());
+                    textView0.setPadding(0,0,10,0);
                     textView0.setText(express_info.getUser());
                     tableRow.addView(textView0);
                     TextView textView1 = new TextView(getApplicationContext());
                     textView1.setText(express_info.getPhone());
+                    textView1.setPadding(0, 0, 10, 0);
                     tableRow.addView(textView1);
                     TextView textView2 = new TextView(getApplicationContext());
                     textView2.setText(express_info.getNumber());
+                    textView2.setPadding(0, 0, 10, 0);
                     tableRow.addView(textView2);
                     TextView textView3 = new TextView(getApplicationContext());
                     textView3.setText(express_info.getCode());
+                    textView3.setPadding(0, 0, 10, 0);
                     tableRow.addView(textView3);
                     TextView textView4 = new TextView(getApplicationContext());
                     textView4.setText(express_info.getExpress());
+                    textView4.setPadding(0,0,10,0);
                     tableRow.addView(textView4);
                     TextView textView5 = new TextView(getApplicationContext());
                     textView5.setText(express_info.getLoc());
